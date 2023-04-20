2d0f84f1f84225b57349f804607d0681625b6385
Steshenko Dmitry <3301297@gmail.com>
2023-04-21 02:51:46 +0300

    refactor: delete temporary buttons

--- src/components/ui/button/Button1.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Button1.module.scss';
-
-const Button1 = ({ children }) => {
-  return <button className={styles.btn}>{children}</button>;
-};
-
-export default Button1;
