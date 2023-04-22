6cbdbc596a6cdbbd1847b78c254451849b8b90c2
Steshenko Dmitry <3301297@gmail.com>
2023-04-22 23:47:07 +0300

    feat: add overview account menu

--- src/pages/account/menu/overview/Overview.tsx
+++ src/pages/account/menu/overview/Overview.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Overview.module.scss';
+import Input from '../../../../components/ui/input/Input';
+import Button from '../../../../components/ui/button/Button';
 
 const Overview = () => {
-  return <section className={styles.overview}>Overview</section>;
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <section className={styles.overview}>
+      <h2 className={styles.title}>Overview</h2>
+      <p className={styles.caption}>
+        Feel free to edit any of your details below so your account is totally
+        up to date.
+      </p>
+      <form className={styles.form}>
+        <Input
+          onChange={setUsername}
+          type="text"
+          className={styles.input}
+          placeholder={'Username'}
+        />
+        <Input
+          onChange={setEmail}
+          type="email"
+          className={styles.input}
+          placeholder={'Email'}
+        />
+        <Input
+          onChange={setPassword}
+          type="password"
+          className={styles.input}
+          placeholder={'Password'}
+        />
+        <Input
+          onChange={setPassword}
+          type="password"
+          className={styles.input}
+          placeholder={'Password'}
+        />
+        <Button onClick={() => {}}>Update</Button>
+      </form>
+    </section>
+  );
 };
 
 export default Overview;
